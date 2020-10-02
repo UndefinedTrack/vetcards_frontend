b85afb615608badd4a578e2b3546f648d4b82427
Оксана Анощенко <foxygen@MacBook-Pro.local>
2020-10-03 00:04:24 +0300

    подготовила проект к разработке

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { HashRouter, Route } from 'react-router-dom'
+import Profile from './Profile'
+import MyPets from './MyPets'
+import Header from './Header'
+import styles from '../styles/App.module.css'
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <HashRouter>
+        <Route exact path="/">
+          <h1>Пользуйтесь нашим великолепным сервисом!</h1>
+        </Route>
+        <Route path="/profile">
+          <Header header="profile" />
+          <Profile />
+        </Route>
+        <Route path="/mypets">
+          <Header header="myPets" />
+          <MyPets />
+        </Route>
+      </HashRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/MyPets.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function MyPets() {
+  return <div>MyPets</div>
+}
+
+export default MyPets
