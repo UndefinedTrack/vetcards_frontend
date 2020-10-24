2ea50bdbb82f850b082125cc73b0d4c6555f8e15
Оксана Анощенко <anoschenko.ov@phystech.edu>
2020-10-24 18:23:46 +0300

    правки для тревиса

--- src/components/vet/CreateVisitForm.js
+++ src/components/vet/CreateVisitForm.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import styles from '../../styles/vet/VisitsHistory.module.css'
