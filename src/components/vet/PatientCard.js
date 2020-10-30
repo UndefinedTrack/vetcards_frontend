177482ea4de17e88fb5c7d7ff47fbdb0334d5bdf
Оксана Анощенко <anoschenko.ov@phystech.edu>
2020-10-30 04:57:30 +0300

    частично адаптировано под ноутбуки

--- src/components/vet/PatientCard.js
+++ src/components/vet/PatientCard.js
@@ -9,13 +9,19 @@ function PatientCard({ patient }) {
   return (
     <div className={styles.PatientsContainer}>
       <Photo className={styles.Photo} />
-      <Info sectionName="Пациент" value={patient.patient} />
-      <Info sectionName="Окрас" value="Окрас" />
-      <Info sectionName="Дата рождения" value="1970-01-01" />
-      <Info sectionName="Пол" value="male" />
-      <Info sectionName="Чип" value="000000000000000" />
-      <Info sectionName="Владелец" value={patient.owner} />
-      <Info sectionName="Номер карты" value={patient.card} />
+      <div className={styles.PatientInfo}>
+        <div className={styles.Column}>
+          <Info sectionName="Пациент" value={patient.patient} />
+          <Info sectionName="Владелец" value={patient.owner} />
+          <Info sectionName="Номер карты" value={patient.card} />
+        </div>
+        {/* <Info sectionName="Окрас" value="Окрас" /> */}
+        <div className={styles.Column}>
+          <Info sectionName="Пол" value={patient.gender} />
+          <Info sectionName="Дата рождения" value={patient.birthDate} />
+          <Info sectionName="Чип" value={patient.chip} />
+        </div>
+      </div>
       <HistoryButton pet={patient} />
     </div>
   )
@@ -32,7 +38,7 @@ function Info({ sectionName, value }) {
   }
   return (
     <div className={styles.InfoContainer}>
-      <div className={`${styles.InfoName} ${styles.InfoText}`}>{sectionName}</div>
+      {value && <div className={`${styles.InfoName} ${styles.InfoText}`}>{sectionName}</div>}
       <div className={`${styles.InfoData} ${styles.InfoText}`}>{sectionData}</div>
     </div>
   )
