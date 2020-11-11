2c70c14c525de0f1a7449cf06801513d1de3b012
Оксана Анощенко <anoschenko.ov@phystech.edu>
2020-11-11 03:52:29 +0300

    реализована регистрация

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ import Schedule from './vet/Schedule'
 import CreateSchedule from './vet/CreateSchedule'
 import VisitsHistory from './vet/VisitsHistory'
 import MainPage from './MainPage'
+import SignUp from './SignUp.js'
 
 function App() {
   const [input, setInput] = useState([])
@@ -22,6 +23,9 @@ function App() {
       <HashRouter>
         <Route exact path="/">
           <MainPage />
+        </Route>
+        <Route exact path="/sign-up">
+          <SignUp />
         </Route>
         <Route path="/profile">
           <Header header="profile" isVet={false} />
