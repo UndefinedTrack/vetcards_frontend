3cdf73c63598caf47496b551b44bf587a316edb9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-07 10:51:37 +0000

    Deploy UndefinedTrack/vetcards_frontend to github.com/UndefinedTrack/vetcards_frontend.git:gh-pages

---
+++ precache-manifest.5ccc204aece0eb0eef9c83190648ce9c.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9329534a8098529526c5dbd601fcb5a",
+    "url": "/vetcards_frontend/index.html"
+  },
+  {
+    "revision": "ada09c5e27c69751fb20",
+    "url": "/vetcards_frontend/static/css/main.18519beb.chunk.css"
+  },
+  {
+    "revision": "0be8108da33f9fbf15ea",
+    "url": "/vetcards_frontend/static/js/2.37f16d95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vetcards_frontend/static/js/2.37f16d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada09c5e27c69751fb20",
+    "url": "/vetcards_frontend/static/js/main.f81a4e52.chunk.js"
+  },
+  {
+    "revision": "c95dd541e2fd0779777d",
+    "url": "/vetcards_frontend/static/js/runtime-main.46d78c4b.js"
+  },
+  {
+    "revision": "88fd821a63088fa74aea0c445a2b0285",
+    "url": "/vetcards_frontend/static/media/VK.88fd821a.svg"
+  },
+  {
+    "revision": "a9d169e0371b8c6bfcf1063fc63ab217",
+    "url": "/vetcards_frontend/static/media/ant-design_mail-outlined.a9d169e0.svg"
+  },
+  {
+    "revision": "bd98cf474c7341859ac0c6bb36b4345b",
+    "url": "/vetcards_frontend/static/media/arrow_back_week.bd98cf47.svg"
+  },
+  {
+    "revision": "abb6693e0831db53cf4b83cf88a93b9c",
+    "url": "/vetcards_frontend/static/media/arrow_down.abb6693e.svg"
+  },
+  {
+    "revision": "de64a3eb02dbf09a7ff9bd620807ebdd",
+    "url": "/vetcards_frontend/static/media/arrow_down_disabled.de64a3eb.svg"
+  },
+  {
+    "revision": "159134ea66478d619af38af3b22b3d6b",
+    "url": "/vetcards_frontend/static/media/arrow_down_square.159134ea.svg"
+  },
+  {
+    "revision": "381c791054ea40852775f8f4e2e567ac",
+    "url": "/vetcards_frontend/static/media/arrow_forward_week.381c7910.svg"
+  },
+  {
+    "revision": "d43c7767f12c5e6f0725d92450eaa31b",
+    "url": "/vetcards_frontend/static/media/arrow_up.d43c7767.svg"
+  },
+  {
+    "revision": "cb2cf206aab05889a6e9b70bb6b0e4e3",
+    "url": "/vetcards_frontend/static/media/back_button.cb2cf206.svg"
+  },
+  {
+    "revision": "0d00b99ca8aa2df58c22570c823c9329",
+    "url": "/vetcards_frontend/static/media/bytesize_book.0d00b99c.svg"
+  },
+  {
+    "revision": "7a831873852a88c64273cca205f363d6",
+    "url": "/vetcards_frontend/static/media/bytesize_settings.7a831873.svg"
+  },
+  {
+    "revision": "e5c4c15856bbd50caf75f2eab069351e",
+    "url": "/vetcards_frontend/static/media/emojione-monotone_yellow-heart.e5c4c158.svg"
+  },
+  {
+    "revision": "a6a41dc31ab0cad8479da6bdf257afa3",
+    "url": "/vetcards_frontend/static/media/et_clock.a6a41dc3.svg"
+  },
+  {
+    "revision": "1005256b51083944cf815f2bb4ceedfd",
+    "url": "/vetcards_frontend/static/media/line-md_computer.1005256b.svg"
+  },
+  {
+    "revision": "684f17f146376d125ff2c72b785853f2",
+    "url": "/vetcards_frontend/static/media/main_owner_1.684f17f1.jpg"
+  },
+  {
+    "revision": "0e1c692b84beb7771f5f3bf2c6054e22",
+    "url": "/vetcards_frontend/static/media/main_owner_2.0e1c692b.jpg"
+  },
+  {
+    "revision": "a87b3fbe843c089158e5dafe8a3c94b6",
+    "url": "/vetcards_frontend/static/media/main_vet_1.a87b3fbe.jpg"
+  },
+  {
+    "revision": "ac49551a393cf7578c1006232eb906b4",
+    "url": "/vetcards_frontend/static/media/main_vet_2.ac49551a.jpg"
+  },
+  {
+    "revision": "4f36debdf9927255521b0cd175411bdd",
+    "url": "/vetcards_frontend/static/media/mdi_keyboard_arrow_left.4f36debd.svg"
+  },
+  {
+    "revision": "3964788bb19fe1e9837ef1a80b175fda",
+    "url": "/vetcards_frontend/static/media/mdi_more_vert.3964788b.svg"
+  },
+  {
+    "revision": "b5e9db2d51dd0ee120b6c3c625bbcee8",
+    "url": "/vetcards_frontend/static/media/photo_pet.b5e9db2d.svg"
+  },
+  {
+    "revision": "c86f5b37bb06e5b4e6a816191cc2b4a6",
+    "url": "/vetcards_frontend/static/media/plus.c86f5b37.svg"
+  },
+  {
+    "revision": "c087e16aa3c587524d873d3f11e192e4",
+    "url": "/vetcards_frontend/static/media/search.c087e16a.svg"
+  },
+  {
+    "revision": "f65c4fb88a891e122a669f41f0dea781",
+    "url": "/vetcards_frontend/static/media/settings.f65c4fb8.svg"
+  },
+  {
+    "revision": "1372c85efcabbeb5f1b2899d098f1821",
+    "url": "/vetcards_frontend/static/media/tabler_brush.1372c85e.svg"
+  },
+  {
+    "revision": "8b9b72a74ca83382f3368e2077de3929",
+    "url": "/vetcards_frontend/static/media/tablet.8b9b72a7.svg"
+  }
+]);
